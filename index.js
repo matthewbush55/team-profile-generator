6bccdf6ad7d5a80e9378831d04b8c06436566d06
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-24 19:32:32 -0600

    Added questions.js file and updated index.html with questions

---
+++ index.js
@@ -0,0 +1,63 @@
+// pull in required libraries
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+// pull in class constructors
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+// pull helper functions
+const htmlGenerator = require("./src/generateHTML");
+const questions = require("./lib/Questions");
+
+// array to hold constructed employee objects
+let teamMembers = [];
+// function to choose an employee type, then call the function for the chosen employee type
+function chooseEmployeeType() {
+  inquirer.prompt(questions.employeeType).then((response) => {
+    switch (response.type) {
+      case "Engineer":
+        getEngineer();
+        break;
+      case "Intern":
+        getIntern();
+        break;
+      case "I'm finished creating employees":
+        console.log("Team creation complete. Generating HTML page...");
+        console.log(teamMembers);
+        break;
+    }
+  });
+}
+// function to prompt for engineer questions
+async function getEngineer() {
+  await inquirer.prompt(questions.engineer).then((response) => {
+    const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
+    teamMembers.push(newEngineer);
+    chooseEmployeeType();
+  });
+}
+
+// function to prompt for intern questions
+async function getIntern() {
+  await inquirer.prompt(questions.intern).then((response) => {
+    const newIntern = new Intern(response.name, response.id, response.email, response.school);
+    teamMembers.push(newIntern);
+    chooseEmployeeType();
+  });
+}
+
+// main function to get user input, starting with the manager since it is required to build a team
+async function init() {
+  await inquirer.prompt(questions.manager).then((response) => {
+    const newManager = new Manager(response.name, response.id, response.email, response.office);
+    teamMembers.push(newManager);
+    chooseEmployeeType();
+  });
+}
+
+// build team and write to HTML file
+
+// initialize app
+init();
